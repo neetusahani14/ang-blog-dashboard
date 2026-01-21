2c633235fa551a4b8d15666d03386f2aa315eea0
neetusahani14 <neetusahani78226@gmail.com>
2026-01-21 11:53:43 +0530

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./layouts/header/header";
+import { Footer } from "./layouts/footer/footer";
+import { Dashboard } from "./dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
