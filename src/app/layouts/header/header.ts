50a827812bde68345a8c8886a75dceb577280eeb
neetusahani14 <neetusahani78226@gmail.com>
2026-02-05 12:10:42 +0530

    commit

--- src/app/layouts/header/header.ts
+++ src/app/layouts/header/header.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Authentication } from '../../services/authentication';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+
+  userEmail: string = '';
+  isLoggedIn$!: Observable<boolean>;
+  constructor(private auth: Authentication) {}
+
+  ngOnInit(): void {
+
+    this.userEmail = JSON.parse(localStorage.getItem('user') || '{}').email;
+    this.isLoggedIn$ = this.auth.isLoggedIn();
+  }
+
+
+  onLogout() {
+    this.auth.logOut();
+  }
 
 }
--- src/app/services/authentication.ts
+++ src/app/services/authentication.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Auth, onAuthStateChanged, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class Authentication {
+
+  loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   constructor(private auth: Auth, private toastr: ToastrService,
     private router: Router
   ) {}
@@ -13,10 +16,37 @@ export class Authentication {
     signInWithEmailAndPassword(this.auth, email, password)
       .then(logRef => {
         this.toastr.success('Login successful!');
+        this.loadUser();
+        this.loggedIn.next(true);
         this.router.navigate(['/']);
       })
       .catch(err => {
         this.toastr.warning(err.message);
       });
   }
+
+   loadUser() {
+    return onAuthStateChanged(this.auth, (user) => {
+      // console.log(JSON.parse(JSON.stringify(user)));
+      localStorage.setItem('user', JSON.stringify(user)); // Store user data in local storage
+
+
+    });
+  }
+
+  logOut() {  
+    this.auth.signOut().then(() => {
+      this.toastr.success('Logout successful!');
+      this.router.navigate(['/login']);
+      localStorage.removeItem('user'); 
+      this.loggedIn.next(false); 
+  }).catch((error) => {
+      console.error('Error signing out: ', error);
+  });
+  }
+
+  isLoggedIn() {
+    return this.loggedIn.asObservable();
+  }
+
 }
